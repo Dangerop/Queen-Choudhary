826e44d076bcab7ebc59409ea7ed16835a535465
Dangerop <115329447+Dangerop@users.noreply.github.com>
2024-07-17 13:01:23 +0530

    Update __init__.py

--- SlayerX/__init__.py
+++ SlayerX/__init__.py
@@ -1,8 +1,8 @@
-from SlayerX.core.bot import Slayer
-from SlayerX.core.dir import dirr
-from SlayerX.core.git import git
-from SlayerX.core.userbot import Userbot
-from SlayerX.misc import dbb, heroku
+from Queen-Choudhary.core.bot import Slayer
+from Queen-Choudhary.core.dir import dirr
+from Queen-Choudhary.core.git import git
+from Queen-Choudhary.core.userbot import Userbot
+from Queen-Choudhary.misc import dbb, heroku
 
 from .logging import LOGGER
 
